4e7c7d1bd658e80c42dc90a308f1339d9a131c9a
creativethumbs <ticha.seth@gmail.com>
2015-03-26 21:24:18 -0400

    fleshed out gui more

--- camomail-gui.py
+++ camomail-gui.py
@@ -1,70 +1,99 @@
 from Tkinter import *
-
-def onclick():
-   pass
+from smtplib import SMTP
+import datetime
 
 root = Tk()
 # so people don't screw around with the window
 root.resizable(0,0)
 
-# text area is placed in the top frame...
 topframe = Frame(root)
 topframe.pack()
-# login frame
-loginframe = Frame(root)
-loginframe.pack()
-
-# ...send button is placed in the bottom
 bottomframe = Frame(root)
 bottomframe.pack(side = BOTTOM)
 
-def loginScreen(): 
-    data = text.get("1.0",'end-1c')
+def loginScreen(msg, subj, from_addr, to_addr):  
+    global msg_str, subj_str, from_str, to_str
+    msg_str = msg
+    subj_str = subj
+    from_str = from_addr
+    to_str = to_addr
+    print subj_str
+    
+    destroyAll()
 
-    # clear previous screen
+    global password_field
+
+    Label(topframe, text="Enter your password below:").grid(row=0)
+
+    password_label = Label(bottomframe, text="Password").grid(row=1)
+    password_field = Entry(bottomframe, show="*")
+    password_field.grid(row=1, column=1, padx=20)
+
+    sendbutton = Button(bottomframe, text="Send Message", command =lambda: sendMsg(password_field.get()))
+    sendbutton.grid(row=2, column=0, ipady=20)
+
+    cancelbutton = Button(bottomframe, text="Cancel", command = messageScreen)
+    cancelbutton.grid(row=2, column=1, ipady=20)
+    
+
+def sendMsg(password_field): 
+    password = password_field
+
+    debuglevel = 0
+
+    smtp = SMTP()
+    smtp.set_debuglevel(debuglevel)
+    smtp.connect('smtp.gmail.com', 465)
+    smtp.login(from_addr, password)
+
+    to_addr = ["ticha.seth@gmail.com"]
+
+    email = """\
+    From: %s
+    To: %s
+    Subject: %s
+
+    %s
+    """ % (from_str, ", ".join(to_addr), subj_str, msg_str)
+
+    smtp.sendmail(from_str, to_addr, email)
+    smtp.quit()
+    destroyAll()
+
+# clear screen
+def destroyAll():
     for child in topframe.winfo_children():
         child.destroy()
     for child in bottomframe.winfo_children():
         child.destroy()
 
-    Label(topframe, text="Enter your email address and password below:").grid(row=0)
-
-    # draws login frame
-    username_label = Label(bottomframe, text="User Name").grid(row=0)
-    username_field = Entry(bottomframe).grid(row=0, column=1, padx=20)
-    
-    password_label = Label(bottomframe, text="Password").grid(row=1)
-    password_field = Entry(bottomframe).grid(row=1, column=1, padx=20)
-
-    sendbutton = Button(bottomframe, text="Send Message", command = messageScreen, fg="black")
-    sendbutton.grid(row=2, column=0, ipady=20)
-
-    cancelbutton = Button(bottomframe, text="Cancel", command = messageScreen, fg="black")
-    cancelbutton.grid(row=2, column=1, ipady=20)
-    
-    print data
-
-
-def destroyAll():
-    for child in topframe.winfo_children():
-        child.destroy()
-
 def messageScreen():
+    destroyAll()
+
     # text area
-    global text
+    global msg, subj, from_addr, to_addr
 
-    scroll = Scrollbar(root)
-    text = Text(topframe, height = 10, width = 60)
-    scroll.pack(side=RIGHT, fill=Y)
-    text.pack(side=LEFT, fill=Y)
-    scroll.config(command=text.yview)
-    text.config(yscrollcommand=scroll.set)
+    from_label = Label(topframe, text="From:")
+    from_label.grid(row=0)
+    from_addr = Entry(topframe)
+    from_addr.grid(row=0, column=1, padx=20)
 
-    text.insert(END, "Type your message here...")
+    to_label = Label(topframe, text="To:")
+    to_label.grid(row=1)
+    to_addr = Entry(topframe)
+    to_addr.grid(row=1, column=1, padx=20)
+    
+    subj_label = Label(topframe, text="Subject:")
+    subj_label.grid(row=2)
+    subj = Entry(topframe)
+    subj.grid(row=2, column=1, padx=20)
+    
+    msg_label = Label(bottomframe, text="Message:").grid(row=0, column=0)
+    msg = Text(bottomframe, height = 10, width = 60, highlightbackground="#D3F2E6")
+    msg.grid(row=1)
 
-    # send button
-    blackbutton = Button(bottomframe, text="Send Message", command = loginScreen, fg="black")
-    blackbutton.pack( side = BOTTOM)
+    sendbutton = Button(bottomframe, text="Next", command = lambda:loginScreen(msg.get("1.0",'end-1c'), subj.get(), from_addr.get(), to_addr.get()))
+    sendbutton.grid(row=2)
 
 messageScreen()
 mainloop()
